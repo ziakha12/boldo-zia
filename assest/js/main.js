ef7309b10d2df76dde8e1c7b00962be476510c6e
ziakha12 <kzzia06@gmail.com>
2023-04-11 15:40:56 +0500

    feat: init project

---
+++ assest/js/main.js
@@ -0,0 +1,16 @@
+
+$('.banner-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 700,
+    autoplay: true,
+    autoplaySpeed: 700,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+  });
+  $('testi-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  });
+  
